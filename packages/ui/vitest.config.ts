595a0ffc70f201e87035236cf0aabf89b9734541
mine <minesunny@icloud.com>
2026-03-27 09:47:53 +0800

    fix(ui): allow vitest to pass with no test files

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    passWithNoTests: true,
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/__tests__/setup.ts'],
